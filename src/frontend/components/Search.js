876804c6ed2239e619ee079c1faffa1b787c85a7
H <684171@pdsb.net>
2022-05-21 13:29:12 -0400

    frontend init commit

---
+++ src/frontend/components/Search.js
@@ -0,0 +1,9 @@
+import styles from './Search.module.scss'
+
+export default function Search() {
+    return (
+        <div id="search">
+            <input type="text" placeholder="Search for a product"></input>
+        </div>
+    )
+}
---
+++ src/frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Search from '../components/Search'
+
+export default function Frugle() {
+  return (
+    <div id="frugle">
+        <div id="title">Frugle</div>
+        <Search></Search>
+    </div>
+  )
+}
