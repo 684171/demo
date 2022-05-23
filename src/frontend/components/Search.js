647610f659e0577d7eeab5ed0c1cfbdb6f845859
H <684171@pdsb.net>
2022-05-23 11:35:07 -0400

    add query state

--- src/frontend/components/Search.js
+++ src/frontend/components/Search.js
@@ -1,7 +1,14 @@
+import { useState } from 'react'
+
 export default function Search() {
+    const [query, setQuery] = useState('')
+    
     return (
-        <div id="search">
-            <input type="text" placeholder="Search a Product"></input>
+        <div id="container">
+            <div id="search">
+                <input type="text" placeholder="Search For A Product" value={query} onChange={(e) => setQuery(e.target.value)}/>
+                <img src="../" />
+            </div>
         </div>
     )
 }
